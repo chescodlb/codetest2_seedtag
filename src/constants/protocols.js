62228bad8c0b2cbc739a44a4a512f511973f690d
Francisco Javier De La Blanca M <franciscojavierdelablancam@Mac-mini-1812.local>
2020-03-08 02:53:44 +0100

    Creation of inicial structure

---
+++ src/constants/protocols.js
@@ -0,0 +1,8 @@
+module.exports = {
+    CLOSEST: 'closest-enemies',
+    FURTHEST: 'furthest-enemies',
+    ASSIST_ALLIES: 'assist-allies',
+    AVOID_CROSSFIRE: 'avoid-crossfire',
+    PRIORITIZE_MECH: 'prioritize-mech',
+    AVOIDMECH: 'avoid-mech'
+}
